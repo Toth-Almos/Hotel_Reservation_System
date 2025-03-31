4d5c9d819ba3eb6a67d0788a55da4bb3ce752960
Almos-Toth <toth.2000.almos@gmail.com>
2025-03-31 23:14:39 +0200

    add Controller for reservation

--- backend/hotelreservationsystem/src/main/java/com/toth_almos/hotelreservationsystem/controller/ReservationController.java
+++ backend/hotelreservationsystem/src/main/java/com/toth_almos/hotelreservationsystem/controller/ReservationController.java
@@ -2,28 +2,43 @@ package com.toth_almos.hotelreservationsystem.controller;
 
 import com.toth_almos.hotelreservationsystem.dto.ReservationDTO;
 import com.toth_almos.hotelreservationsystem.dto.ReservationRequest;
+import com.toth_almos.hotelreservationsystem.mapper.ReservationMapper;
+import com.toth_almos.hotelreservationsystem.model.Reservation;
 import com.toth_almos.hotelreservationsystem.service.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/reservation")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ReservationController {
     private final ReservationService reservationService;
+    private final ReservationMapper reservationMapper;
 
     @Autowired
-    public ReservationController(ReservationService reservationService) {
+    public ReservationController(ReservationService reservationService, ReservationMapper reservationMapper) {
         this.reservationService = reservationService;
+        this.reservationMapper = reservationMapper;
     }
 
     @PostMapping("/create-reservation")
     public ResponseEntity<ReservationDTO> createReservation(ReservationRequest reservationRequest) {
-        //TODO
-        return ResponseEntity.ok(new ReservationDTO());
+        Reservation reservation = reservationService.createReservation(reservationRequest);
+        return ResponseEntity.ok(reservationMapper.toDTO(reservation));
+    }
+
+    @DeleteMapping("/delete-reservation/{id}")
+    public ResponseEntity<String> deleteReservation(@PathVariable("id") Long id) {
+        reservationService.deleteReservation(id);
+        return ResponseEntity.ok("Successfully deleted reservation.");
+    }
+
+    @GetMapping("/get-reservations/{id}")
+    public ResponseEntity<List<ReservationDTO>> getAllReservationsForCustomer(@PathVariable("id") Long id) {
+        List<Reservation> reservations = reservationService.findByCustomerId(id);
+        return ResponseEntity.ok(reservationMapper.toDTOList(reservations));
     }
 }
