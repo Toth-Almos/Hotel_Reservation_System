3a198f9cd178823adc872b4c8a3b2d858497adff
Almos-Toth <toth.2000.almos@gmail.com>
2025-04-06 15:13:26 +0200

    enforce rules for leaving Reviews

--- backend/hotelreservationsystem/src/main/java/com/toth_almos/hotelreservationsystem/service/ReviewServiceImpl.java
+++ backend/hotelreservationsystem/src/main/java/com/toth_almos/hotelreservationsystem/service/ReviewServiceImpl.java
@@ -3,8 +3,10 @@ package com.toth_almos.hotelreservationsystem.service;
 import com.toth_almos.hotelreservationsystem.dto.ReviewRequest;
 import com.toth_almos.hotelreservationsystem.model.Customer;
 import com.toth_almos.hotelreservationsystem.model.Hotel;
+import com.toth_almos.hotelreservationsystem.model.Reservation;
 import com.toth_almos.hotelreservationsystem.model.Review;
 import com.toth_almos.hotelreservationsystem.repository.HotelRepository;
+import com.toth_almos.hotelreservationsystem.repository.ReservationRepository;
 import com.toth_almos.hotelreservationsystem.repository.ReviewRepository;
 import com.toth_almos.hotelreservationsystem.repository.UserRepository;
 import jakarta.persistence.EntityNotFoundException;
@@ -12,19 +14,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class ReviewServiceImpl implements ReviewService {
     private final ReviewRepository reviewRepository;
     private final UserRepository userRepository;
     private final HotelRepository hotelRepository;
+    private final ReservationRepository reservationRepository;
 
     @Autowired
-    public ReviewServiceImpl(ReviewRepository reviewRepository, UserRepository userRepository, HotelRepository hotelRepository) {
+    public ReviewServiceImpl(ReviewRepository reviewRepository, UserRepository userRepository, HotelRepository hotelRepository, ReservationRepository reservationRepository) {
         this.reviewRepository = reviewRepository;
         this.userRepository = userRepository;
         this.hotelRepository = hotelRepository;
+        this.reservationRepository = reservationRepository;
     }
 
     @Override
@@ -38,6 +44,25 @@ public class ReviewServiceImpl implements ReviewService {
         int rating = request.getRating();
         if(rating > 5 || rating < 1) {
             throw new IllegalArgumentException("The rating's numeric values must be between 1 and 5.");
+        }
+
+        List<Review> pastReviews = reviewRepository.findByCustomerId(request.getCustomerId());
+        for(Review review : pastReviews) {
+            if(Objects.equals(review.getHotel().getId(), request.getHotelId())) {
+                throw new IllegalArgumentException("You can only leave one review per hotel.");
+            }
+        }
+
+        boolean reviewEligible = false;
+        List<Reservation> reservations = reservationRepository.findByCustomerId(request.getCustomerId());
+        for(Reservation res : reservations) {
+            if(Objects.equals(res.getHotel().getId(), request.getHotelId()) && LocalDate.now().isAfter(res.getCheckOutDate())) {
+                reviewEligible = true;
+                break;
+            }
+        }
+        if(!reviewEligible) {
+            throw new IllegalArgumentException("You can't rate a hotel in which you were not a quest.");
         }
 
         Customer customer = userRepository.findByCustomerId(request.getCustomerId()).orElseThrow(() -> new EntityNotFoundException("Customer not found"));
